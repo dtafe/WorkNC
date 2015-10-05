d372a71640764dbd651f4d3b3d65568fc75fac69
thienchuong9x <thienchuong.la91@gmail.com>
2015-10-05 17:25:59 +0700

    2

---
+++ WorkNCInfoService.Mvc5/Controllers/WorkZoneController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkNCInfoService.Mvc5.Models.WorkModels;
+
+namespace WorkNCInfoService.Mvc5.Controllers
+{
+    public class WorkZoneController : Controller
+    {
+        WorkContext db = new WorkContext();
+        // GET: WorkZone
+        public ActionResult Index()
+        {
+            return View(db.WorkNC_WorkZone.ToList());
+        }
+
+        // GET: WorkZone/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: WorkZone/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WorkZone/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WorkZone/Edit/5
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WorkZone/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WorkZone/Delete/5
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WorkZone/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- WorkNCInfoService.Mvc5/Models/WorkModels/WorkNC_WorkZone.cs
+++ WorkNCInfoService.Mvc5/Models/WorkModels/WorkNC_WorkZone.cs
@@ -9,19 +9,23 @@ namespace WorkNCInfoService.Mvc5.Models.WorkModels
     public partial class WorkNC_WorkZone
     {
         [Key]
+        
         public int WorkZoneId { get; set; }
 
         [StringLength(100)]
+        [Display(Name = "WorkZone Name")]
         public string Name { get; set; }
 
         public int CompanyId { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Company")]
         public string CompanyName { get; set; }
 
         public int FactoryId { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Factory")]
         public string FactoryName { get; set; }
 
         public int MachineId { get; set; }
@@ -30,28 +34,37 @@ namespace WorkNCInfoService.Mvc5.Models.WorkModels
         public string WorkZonePath { get; set; }
 
         [StringLength(100)]
+        [Display(Name = "Model Data Programer")]
         public string ModelDataProgramer { get; set; }
 
         [StringLength(100)]
+        [Display(Name = "NC Data Programer")]
         public string NCDataProgramer { get; set; }
 
+        [Display(Name = "Program date")]
         public DateTime? ProgramDate { get; set; }
 
         [StringLength(100)]
+        [Display(Name = "Model name")]
         public string ModelName { get; set; }
 
         [StringLength(100)]
+        [Display(Name = "Parts")]
         public string Parts { get; set; }
 
         [StringLength(100)]
+        [Display(Name = "Parts Name")]
         public string PartName { get; set; }
 
         [StringLength(10)]
+        [Display(Name = "Machining Time Total")]
         public string MachiningTimeTotal { get; set; }
 
         [StringLength(255)]
+        [Display(Name = "Comment")]
         public string Comment { get; set; }
 
+        [Display(Name = "Status")]
         public int Status { get; set; }
 
         [StringLength(250)]
